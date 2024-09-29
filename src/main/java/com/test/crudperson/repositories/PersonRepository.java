3c0d053124d730bfc76b16ef86ae85af69a4d692
merieme04 <meriemeanasri04@gmail.com>
2024-09-29 04:24:03 +0100

    add search by name end pagination in backend

--- src/main/java/com/test/crudperson/repositories/PersonRepository.java
+++ src/main/java/com/test/crudperson/repositories/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.test.crudperson.repositories;
 
 import com.test.crudperson.entities.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<Person> findByNom(String nom);
+    Page<Person> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCase(String nom, String prenom, Pageable pageable);
 }
